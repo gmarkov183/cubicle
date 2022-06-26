a20edbe5a40de5e9a7c0a4487a0c430838eb68de
Georgi Markov <markov8111@gmail.com>
2022-06-26 20:24:46 +0300

    Make a relation between cube and accessory

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const Accessory = require("../models/Accessory");
 
 const Cube = require("../models/Cube");
 
@@ -22,3 +23,16 @@ exports.getOne = (cubeId) => Cube.findById(cubeId);
 exports.create = (cube) => {
   return Cube.create(cube);
 };
+
+exports.attachAccessory = async(cubeId, accessoryId) => {
+  const cube = await  Cube.findById(cubeId);
+  const accessory = await Accessory.findById(accessoryId);
+
+  cube.accessories.push(accessory);
+  accessory.cubes.push(cube);
+
+  await cube.save();
+  await accessory.save();
+
+  return cube;
+}
